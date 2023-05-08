ca48e0bb9ec67564baf390c7925864356048c53b
Martin <meatole.cs@gmail.com>
2023-05-09 00:41:24 +0800

    Update: made pictures more dynamic

--- tactics-client/app/Http/Controllers/UserController.php
+++ tactics-client/app/Http/Controllers/UserController.php
@@ -6,5 +6,20 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+     public function uploadProfilePicture(Request $request)
+    {
+        $request->validate([
+            'profile_picture' => 'image|mimes:jpeg,png,jpg,gif|max:2048',
+        ]);
+
+        if ($request->hasFile('profile_picture')) {
+            $profilePicture = $request->file('profile_picture');
+            $path = $profilePicture->store('profile_pictures', 'public');
+
+            // Save the file path or filename in the user model
+            auth()->user()->update(['profile_picture' => $path]);
+        }
+
+        return redirect()->back()->with('success', 'Profile picture uploaded successfully.');
+    }
 }
