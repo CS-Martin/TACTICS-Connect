94174589f5b42cd22435980d321d5681c0fac94c
Martin <meatole.cs@gmail.com>
2023-05-10 20:32:45 +0800

    Fix: comments will now display profile pictures from their own user/owner

--- tactics-client/app/Models/Comment.php
+++ tactics-client/app/Models/Comment.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use App\Models\Post;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
@@ -12,7 +13,12 @@ class Comment extends Model
     // Define the relationship to post
     public function post()
     {
-        
+
         return $this->belongsTo(Post::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
     }
 }
